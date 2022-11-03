64005bac9f8e645e6ff5a6af412b08b5cb93dd3b
Ollie Willette <wille667@morris.umn.edu>
2022-11-03 15:17:04 -0500

    Add enough so it dort of works. Crashes, I have no idea why.
    Honestly I have no idea why it works in the first place. Which makes it hard to debug/feel good about myself.

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -14,15 +14,66 @@ public class EchoClient {
 	}
 
 	private void start() throws IOException {
-		Socket socket = new Socket("localhost", PORT_NUMBER);
-		InputStream socketInputStream = socket.getInputStream();
-		OutputStream socketOutputStream = socket.getOutputStream();
+		try (Socket socket = new Socket("localhost", PORT_NUMBER)) {
+			InputStream socketInputStream = socket.getInputStream();
+			OutputStream socketOutputStream = socket.getOutputStream();
 
-		// Put your code here. 
-		InThread inThread = new InThread(socketOutputStream);
-		Thread Tin = new Thread(inThread);
+			// Put your code here.
+			InThread inThread = new InThread(socketOutputStream);
+			Thread Tin = new Thread(inThread);
+			Tin.start();
 
-		OutThread outThread = new OutThread(socketInputStream);
-		Thread Tout = new Thread(outThread);
+			OutThread outThread = new OutThread(socketInputStream);
+			Thread Tout = new Thread(outThread);
+			Tout.start();
+		}
+	}
+
+	public class OutThread implements Runnable {
+
+		private InputStream socket;
+
+		public OutThread(InputStream socketInputStream) {
+			socket = socketInputStream;
+		}
+
+		@Override
+		public void run() {
+			try {
+				while (true) {
+					System.out.write(socket.read());
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	public class InThread implements Runnable {
+
+		private OutputStream socketOutputStream;
+
+		public InThread(OutputStream socketOutputStream) {
+			this.socketOutputStream = socketOutputStream;
+		}
+
+		@Override
+		public void run() {
+			int data;
+			try {
+				data = System.in.read();
+				while (data >= 0) {
+					socketOutputStream.write(data);
+					data = System.in.read();
+				}
+				socketOutputStream.flush();
+				socketOutputStream.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+
 	}
 }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -7,25 +7,55 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-	
+
 	// REPLACE WITH PORT PROVIDED BY THE INSTRUCTOR
-	public static final int PORT_NUMBER = 0; 
+	public static final int PORT_NUMBER = 6013;
+
 	public static void main(String[] args) throws IOException, InterruptedException {
 		EchoServer server = new EchoServer();
 		server.start();
 	}
 
 	private void start() throws IOException, InterruptedException {
-		ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
-		while (true) {
-			Socket socket = serverSocket.accept();
+		try (ServerSocket serverSocket = new ServerSocket(PORT_NUMBER)) {
+			while (true) {
+				Socket socket = serverSocket.accept();
 
-			// Put your code here.
-			// This should do very little, essentially:
-			//   * Construct an instance of your runnable class
-			//   * Construct a Thread with your runnable
-			//      * Or use a thread pool
-			//   * Start that thread
+				// Put your code here.
+				// This should do very little, essentially:
+				// * Construct an instance of your runnable class
+				// * Construct a Thread with your runnable
+				// * Or use a thread pool
+				// * Start that thread
+				ServerRunnable serverRunnable = new ServerRunnable(socket);
+				Thread thread = new Thread(serverRunnable);
+				thread.start();
+			}
+		}
+	}
+
+	public class ServerRunnable implements Runnable {
+		Socket client;
+
+		public ServerRunnable(Socket socket) {
+			client = socket;
+		}
+
+		@Override
+		public void run() {
+			try {
+				InputStream input = client.getInputStream();
+				OutputStream output = client.getOutputStream();
+				int data = input.read();
+				while (data >= 0) {
+					output.write(data);
+					data = input.read();
+				}
+				output.flush();
+				client.shutdownOutput();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
